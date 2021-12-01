102ae6abdec5ad67bdc5660ccf766d62174a71e8
Yigit Dallilar <yigit.dallilar@gmail.com>
2021-12-01 21:52:13 +0100

    init

---
+++ .github/workflows/actions/build_manylinux/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+yum -y install gsl-devel
+
+PYVER={$1#.}
+
+case PYVER in
+
+	35 | 36 | 37)
+		PYTHON_VERSION=cp${PYVER}-cp${PYVER}m
+		;;
+	38 | 39 | 310)
+		PYTHON_VERSION=cp${PYVER}-cp${PYVER}
+		;;
+	*)
+		echo OOPS
+		;;
+esac
+
+/opt/python/${PYTHON_VERSION}/bin/pip install --upgrade pip
+/opt/python/${PYTHON_VERSION}/bin/pip install -U wheel auditwheel
+/opt/python/${PYTHON_VERSION}/bin/pip install -U cython numpy
+/opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel -d wheelhouse
+
+auditwheel repair /github/workspace/wheelhouse/*.whl
+rm /github/workspace/wheelhouse/*.whl
+
+
+
