25dcd2279f107ba0e50162207f134bbf23e60e12
Yigit Dallilar <yigit.dallilar@gmail.com>
2021-12-01 22:56:57 +0100

    Update entrypoint.sh

--- .github/workflows/actions/build_manylinux/entrypoint.sh
+++ .github/workflows/actions/build_manylinux/entrypoint.sh
@@ -26,7 +26,7 @@ echo ${PYTHON_VERSION}
 /opt/python/${PYTHON_VERSION}/bin/pip install -U cython numpy
 /opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel -d wheelhouse
 
-WHEEL=/$(ls wheelhouse/*.whl)
+WHEEL=$(ls wheelhouse/*.whl)
 auditwheel repair $WHEEL
 rm $WHEEL
 
